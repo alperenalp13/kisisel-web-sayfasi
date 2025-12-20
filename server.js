5c5c916baa8a88b7ec7be12c35625125f094ee0f
alperenalp13 <alperenalp216@gmail.com>
2025-12-20 16:15:39 +0300

    Add explicit root route to prevent undefined errors

--- server.js
+++ server.js
@@ -27,6 +27,10 @@ const authenticate = (req, res, next) => {
 };
 
 // --- Routes ---
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'login.html'));
 });
