964a78c771d549f812c00ca32c8c9926e13fa312
alperenalp13 <alperenalp216@gmail.com>
2026-01-08 14:02:29 +0300

    Render için port yapılandırması güncellendi

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { v4: uuidv4 } = require('uuid');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Basit bir "In-Memory" session deposu
 const sessions = {}; 
